f15faa7b7866c6183e4d3210e28334ddad5a52c2
BiscuiTech <jhuet@biscui.tech>
2022-07-05 21:30:38 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,65 @@
-mod todo;
 mod io;
-use io::read_data;
+mod todo;
+use std::process;
 
+use io::read_data;
+use ncurses::{ll::wget_wch, *};
 
 fn main() {
-    println!("Tasky!");
-    println!("-----");
-
     // call read_data
     let content = read_data();
     // iterate through the vector of Todos
-    for todo in content {
-        println!("{}", todo.print());
+    // for todo in content {
+    //    todo.print()
+    // }
+    /* If your locale env is unicode, you should use `setlocale`. */
+    // let locale_conf = LcCategory::all;
+    // setlocale(locale_conf, "zh_CN.UTF-8"); // if your locale is like mine(zh_CN.UTF-8).
+
+    /* Start ncurses. */
+    initscr();
+
+    /* Print to the back buffer. */
+    addstr("Tasky!\n");
+    addstr("(q) quit (a) add (r) rename (t) toggle (d) delete\n");
+    hline(ACS_HLINE(), COLS());
+
+    /* Print some unicode(Chinese) string. */
+    // addstr("Great Firewall dislike VPN protocol.\nGFW 不喜欢 VPN 协议。");
+
+    /* Wait for input. */
+    let ch = wget_wch(stdscr());
+    match ch {
+        Some(WchResult::KeyCode(q)) => {
+            process::exit(0);
+        }
+
+        // Some(WchResult::KeyCode(_)) => {
+        //     /* Enable attributes and output message. */
+        //     attron(A_BOLD | A_BLINK);
+        //     addstr("\nKeycode");
+        //     attroff(A_BOLD | A_BLINK);
+        //     addstr(" pressed");
+        // }
+
+        // Some(WchResult::Char(c)) => {
+        //     /* Enable attributes and output message. */
+        //     addstr("\nKey pressed: ");
+        //     attron(A_BOLD | A_BLINK);
+        //     addstr(format!("{}\n", char::from_u32(c as u32).expect("Invalid char")).as_ref());
+        //     attroff(A_BOLD | A_BLINK);
+        // }
+        None => {
+            addstr("\nYou didn't enter a character in time!");
+        }
     }
+
+    /* Update the screen. */
+    refresh();
+
+    /* Wait for a key press. */
+    getch();
+
+    /* Terminate ncurses. */
+    endwin();
 }
--- src/todo.rs
+++ src/todo.rs
@@ -1,26 +1,29 @@
+use std::io;
+use std::io::prelude::*;
+
 pub struct Todo {
-  title: String,
-  completed: bool,
-  // id: u32,
+    title: String,
+    completed: bool,
+    // id: u32,
 }
 
 impl Todo {
-  pub fn new(title: String, completed: bool) -> Todo {
-      Todo {
-          title,
-          completed,
-          // id: id,
-      }
-  }
-  pub fn print(&self) -> String {
-      // if completed, return "[x] " + title
-      let completed = if self.completed { 'x' } else { ' ' };
-      format!("[{}] - {}", completed, self.title)
-  }
-  pub fn toggle(&mut self) {
-      self.completed = !self.completed;
-  }
-  pub fn rename(&mut self, new_title: String) {
-      self.title = new_title;
-  }
+    pub fn new(title: String, completed: bool) -> Todo {
+        Todo {
+            title,
+            completed,
+            // id: id,
+        }
+    }
+    pub fn print(&self) {
+        let completed = if self.completed { "[X]" } else { "[ ]" };
+        let string = completed.to_string() + " " + &self.title + "\n";
+        io::stdout().write_all(string.as_bytes()).unwrap();
+    }
+    pub fn toggle(&mut self) {
+        self.completed = !self.completed;
+    }
+    pub fn rename(&mut self, new_title: String) {
+        self.title = new_title;
+    }
 }
