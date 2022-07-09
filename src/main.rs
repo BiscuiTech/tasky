0dab138577405d482bccbda15632fa96bd6440da
BiscuiTech <jhuet@biscui.tech>
2022-07-08 23:10:48 -0400

    Switched to termion

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
 mod io;
 mod todo;
-use std::process;
-
 use io::read_data;
-use ncurses::{ll::wget_wch, *};
 
 fn main() {
     // call read_data
@@ -12,54 +13,44 @@ fn main() {
     // for todo in content {
     //    todo.print()
     // }
-    /* If your locale env is unicode, you should use `setlocale`. */
-    // let locale_conf = LcCategory::all;
-    // setlocale(locale_conf, "zh_CN.UTF-8"); // if your locale is like mine(zh_CN.UTF-8).
 
-    /* Start ncurses. */
-    initscr();
+    // Initialize 'em all.
+    let stdout = stdout();
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+    let stdin = stdin();
+    let stdin = stdin.lock();
 
-    /* Print to the back buffer. */
-    addstr("Tasky!\n");
-    addstr("(q) quit (a) add (r) rename (t) toggle (d) delete\n");
-    hline(ACS_HLINE(), COLS());
+    write!(
+        stdout,
+        "{}{}{}Tasky! | 'q' will exit.{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::style::Bold,
+        termion::style::Reset,
+        termion::cursor::Goto(2, 2)
+    )
+    .unwrap();
+    stdout.flush().unwrap();
 
-    /* Print some unicode(Chinese) string. */
-    // addstr("Great Firewall dislike VPN protocol.\nGFW 不喜欢 VPN 协议。");
+    let mut keys = stdin.keys();
+    loop {
+        let c = keys.next().unwrap().unwrap();
 
-    /* Wait for input. */
-    let ch = wget_wch(stdscr());
-    match ch {
-        Some(WchResult::KeyCode(q)) => {
-            process::exit(0);
+        match c {
+            // Quit
+            Key::Char('q') => return,
+            // Clear the screen
+            Key::Char('c') => write!(stdout, "{}", termion::clear::All),
+            _ => write!(
+                stdout,
+                "{}{}{:?}",
+                termion::clear::CurrentLine,
+                termion::cursor::Goto(2, 2),
+                c
+            ),
         }
+        .unwrap();
 
-        // Some(WchResult::KeyCode(_)) => {
-        //     /* Enable attributes and output message. */
-        //     attron(A_BOLD | A_BLINK);
-        //     addstr("\nKeycode");
-        //     attroff(A_BOLD | A_BLINK);
-        //     addstr(" pressed");
-        // }
-
-        // Some(WchResult::Char(c)) => {
-        //     /* Enable attributes and output message. */
-        //     addstr("\nKey pressed: ");
-        //     attron(A_BOLD | A_BLINK);
-        //     addstr(format!("{}\n", char::from_u32(c as u32).expect("Invalid char")).as_ref());
-        //     attroff(A_BOLD | A_BLINK);
-        // }
-        None => {
-            addstr("\nYou didn't enter a character in time!");
-        }
+        stdout.flush().unwrap();
     }
-
-    /* Update the screen. */
-    refresh();
-
-    /* Wait for a key press. */
-    getch();
-
-    /* Terminate ncurses. */
-    endwin();
 }
